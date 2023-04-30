bdab09e01dec637fd1c3ef282677002acd9421f1
Zarturia <fallgame994@gmail.com>
2023-05-01 00:42:01 +0800

    hover ni jm

--- java.js
+++ java.js
@@ -2,22 +2,30 @@
 // Array of image URLs
 var images = ["./Photo/mainimg2.png", "./Photo/mainimg3.png", "./Photo/mainimg4.png"];
 var currentIndex = 0; 
-
-
 function changeImage() {
   var img = document.getElementById("Rec1img");
   img.src = images[currentIndex];  
   
   currentIndex++; 
-  
 
   if (currentIndex >= images.length) {
     currentIndex = 0;
   }
 }
-
-
 var interval = 5000;  // Change image every 5 seconds
-
-
 setInterval(changeImage, interval);
+
+//Hover image
+function CakeHov(element, newImage) {
+    element.src = newImage;
+}
+
+//Hover image
+function PasHov(element, newImage) {
+    element.src = newImage;
+}
+
+//Hover image
+function BreadHov(element, newImage) {
+    element.src = newImage;
+}
