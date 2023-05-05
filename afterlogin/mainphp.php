803fe5767142f8888bf66a4fd3383ca0ad0b7d90
Fallzur <Tuzarjmt@gmail.com>
2023-05-05 09:49:41 +0800

    bot

---
+++ afterlogin/mainphp.php
@@ -0,0 +1,217 @@
+<?php
+// Include the first file
+include 'file1.php';
+
+// Call the function from the first file
+myFunction();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./stylish.css">
+  <link rel="stylesheet" href="./all.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="java.js"></script>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Custom Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gidugu:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gwendolyn:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Halant:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IM FELL English:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;600;700&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Charm:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fjalla One:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant Infant:wght@700&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cookie:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin Slab:wght@700&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Habibi:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gupter:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant:wght@400&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant Garamond:wght@400&display=swap">
+
+<title>Your Website</title>
+
+</head>
+
+<body>
+  <!-- HTML code-->
+
+  <div class="user-login">
+    <i class='fa fa-user' style="font-size:5vw;" ></i>
+  </div>
+
+  <div class="Ordernow1" style="font-size:1.8vw;" >Order Online</div>
+
+  <div class="Container">
+  <img class="s-cakes-logo-1" alt="" src="./Photo/scakelogo.png"/>
+  <img class="cake-main" id="Rec1img" alt="" src="./Photo/cakemain.png" />
+  <img class="vector-icon" alt="" src="./SVG File/dashpink.svg" />
+  
+  <div class="promo2">
+  <div class="about-us" style="font-size:1.2vw;">Promo</div>
+  </div>
+  
+  <div class="customize-us">
+    <div class="customize-us1" style="font-size:1.2vw;" >Customize </div>
+  </div>
+
+  <div class="est-2013"  style="font-size:0.5vw;">E.S.T 2013</div>
+
+  <button class="Findus">
+    <div class="about-us" style="font-size:1.2vw;" >Find Us</div>
+  </button>
+
+  <button class="aboutuus">
+    <div class="about-us" style="font-size:1.2vw;" >About Us</div>
+  </button>
+
+  <h1 class="tscakes" style="font-size:5vw;">TScakes</h1>
+  <h2 class="cakes-bread" style="font-size:1.4vw;" >Cakes, Bread & Pastries</h2>
+
+  <img class="arrow-icon" alt="" src="./SVG File/arrow.svg" />
+
+<div>
+  <div class="cake-bread" style="font-size:10;" >Cake, Bread & Pastries</div>
+</div>
+  <div class="caken-nidri"style="font-size:2vw;">Cake vary for any event</div>
+
+  <div class="caken-varyni"style="font-size:2vw;">Weddings, Birthdays, Baby showers, Anniversaries, Christmas,
+    Fathers day, Mothers day & Milestones.</div>
+ 
+  <div class="addrssbelow"style="font-size:1.5vw;">Libertad Compound | 123 Location St. example , BXU 92314</div>
+  
+
+  <img class="for-Lin1" alt="" src="./SVG File/Line1.svg" />
+  <img class="for-Lin2" alt="" src="./SVG File/Line2.svg" />
+  <img class="for-Lin3" alt="" src="./SVG File/Line2.svg" />
+
+  <div class="categories1">
+    <div class="Main-3" id="categoriesText" style="font-size:1.8vw;">Categories</div>
+  </div>
+
+  <div class="Promo1">
+    <div class="Main-2" id="categoriesText" style="font-size:1.8vw;">Promo</div>
+  </div>
+
+  <div class="Customize1">
+    <div class="Main-1" id="categoriesText" style="font-size:1.8vw;">Customize</div>
+  </div>
+
+  <div class="Monday" id="categoriesText" style="font-size:1.5vw;">Monday to Friday 8am - 9pm</div>
+  <div class="Saturday" id="categoriesText" style="font-size:1.5vw;">Saturday 8am - 9pm</div>
+  <div class="Sunday" id="categoriesText" style="font-size:1.5vw;">Sunday 8am - 9pm</div>
+
+
+  <div class="JoinUs" id="categoriesText"style="font-size:1.2vw;"><b> Join us to explore exceptional Cakes, Bread & Pastries. A
+    collaboration all cake makers in butuan city, our sensational ZTS
+    Team and a collection of community members and friends.
+    Festivities commence around Breadk, Cakes and Pastries. To save
+    your time by ordering to us and picking up at the exact date
+    given, please go to the link below to order the special cake.
+  <p> We look forward to seeing you pick up our cake!</p></b></div>
+
+  <div class="sco1" id="scotext">
+  <b class="special-cake-offer">Special Cake Offer</b>
+</div>
+
+<img class="event-cakes" id="eventcakess" alt=""src="./Photo/Eventcakes.png"/>
+<div class="wedding-cake"style="font-size:2vw;" id="varycake">WEDDING CAKE</div>
+
+<div class="sweetness-in-every" style="font-size:4.5vw;">
+  “Sweetness in Every Bite, Wonder in Every Slice”
+</div>
+<div class="PLine1" size="20" width="100%"></div>  
+<div class="PLine2" size="20" width="100%"></div>
+<div class="PLine3" size="20" width="100%"></div>
+
+<div class="Categ-Big"style="font-size:3vw;">CATEGORIES</div>
+
+<div class="pastriesn">
+  <img class="Cake-C" alt="" src="./Photo/Cake-C.png" id="rectangleImage2"onmouseover="CakeHov(this, './Photo/Cake-D.png')" onmouseout="CakeHov(this, './Photo/Cake-C.png')"/>
+</div>
+<h1 class="Caketxt" style="font-size:3vw;">CAKES</h1>
+<div class="pastriesn">
+  <img
+    class="Pastrien-C" alt="" src="./Photo/Pastries-C.png" id="rectangleImage2"onmouseover="PasHov(this, './Photo/Pastries-D.png')" onmouseout="PasHov(this, './Photo/Pastries-C.png')"/>
+</div>
+<h1 class="Pastriestxt" style="font-size:3vw;">PASTRIES</h1>
+<div class="pastriesn">
+  <img
+    class="Bread-C" alt="" src="./Photo/Bread-C.png" id="rectangleImage2"onmouseover="BreadHov(this, './Photo/Bread-D.png')" onmouseout="BreadHov(this, './Photo/Bread-C.png')"/>
+</div>
+<h1 class="Breadtxt" style="font-size:3vw;">BREAD</h1>
+
+<div class="news-events"style="font-size:3vw;">News & Events</div>
+<div class="keep-up-to"style="font-size:2vw;">
+  Keep up to date with our latest news. To be the first to find out:
+  subscribe
+</div>
+
+<img class="N1" alt="" src="./Photo/N1.png"/>
+<img class="N2" alt="" src="./Photo/N2.png "/>
+<img class="N3" alt="" src="./Photo/N3.png "/>
+
+<div class="Info1"style="font-size:1.5vw;">
+  A new cake design was created
+</div>
+  <div class="Date1"style="font-size:1.2vw;">September 22, 2023</div>
+
+  <div class="Info2"style="font-size:1.5vw;">
+    Branched out at Libertad Butuan City! Come and visit
+  </div>
+    <div class="Date2"style="font-size:1.2vw;">October 10, 2023</div>
+
+    <div class="Info3"style="font-size:1.5vw;">
+      ZTS techdev has been awarded for creating TScakes website for
+      “The most -----”
+    </div>
+      <div class="Date3"style="font-size:1.2vw;">December 19, 2023</div>
+
+      <div class="BLine4" size="20" width="100%"></div>
+      <div class="BLine5" size="20" width="100%"></div>
+
+            <div class="locpound"style="font-size:1.5vw;">
+              Libertad Compound | 123 Location St. example ,
+              BXU 92314
+            </div>
+
+            <div class="numts"style="font-size:1.5vw;">
+              09123456789 | TScakes@gmail.com
+            </div>
+
+            <div class="dates"style="font-size:1.5vw;">
+              Monday to Saturday 8am - 9pm | Sunday 9am - 9pm
+            </div>
+
+        <div class="find-us"style="font-size:1.8vw;">Find Us</div>
+        <div class="links"style="font-size:1.8vw;">LINKS</div>
+  
+          <div class="BLine5" size="20" width="100%"></div>
+          <div class="Subs"style="font-size:3vw;">Subscribe</div>
+          <div class="sign-up-with"style="font-size:1.5vw;"> Sign up with your email address to receive news and updates.</div>
+          <div class="we-respect-your">We respect your privacy.</div>
+
+      <button class="sign-up"style="font-size:1.1vw;">Sign up</button>
+    <div>
+      <h1 class="zts-techdev-we"style="font-size:1.1vw;">ZTS Techdev “We Create” V.1 First Creation</h1>
+    </div>
+      <input class="rectangle-input" type="text"  placeholder="Your Email Here"style="font-size:1.1vw;"/>
+
+      <div class="BLine6" size="20" width="100%"></div>
+
+  <!-- Bootstrap JavaScript -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  
+
+</body>
