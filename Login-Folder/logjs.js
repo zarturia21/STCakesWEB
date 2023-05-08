53fc3d13c16e295bf32295fbc894a0505a569935
Zarturia <fallgame994@gmail.com>
2023-05-09 00:43:29 +0800

    m

--- Login-Folder/logjs.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("mypasswrod");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
