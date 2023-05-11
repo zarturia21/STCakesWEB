c3fb9803be9539b278552c9a6c6991631a92134d
Zarturia <fallgame994@gmail.com>
2023-05-11 08:26:51 +0800

    s

---
+++ AboutUs-Folder/PhpAboutus/aboutus.php
@@ -0,0 +1,109 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html>
+  <head>
+    
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./new.css" />
+<link rel="stylesheet" href="./global.css" />
+<script src="Jswebse.js"></script>
+
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gidugu:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gwendolyn:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Halant:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=IM FELL English:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;600;700&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Charm:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Fjalla One:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant Infant:wght@700&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cookie:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Josefin Slab:wght@700&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Habibi:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gupter:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant Garamond:wght@400&display=swap"/>
+
+</head>
+
+<body>
+    <div class="boader" size="5"></div>
+    <a href="/Main.html">
+    <div class="shop">Shop</div>
+    </a>
+
+    <img class="search"src="./newphoto/search.png" alt="Search" sizes="-10">
+    <img class="marble"src="./newphoto/marble.png" alt="marbs">
+
+    <div class="trans" size="15"></div>
+    <a href="/Categories-Folder/Categories.html">
+    <div class="functionbut" size="15"></div></a>
+    
+    <div class="about">ABOUT TSCakes</div>
+    <div class="tagline">"Sweetness in Every Bite, Wonder in Every Slice"</div>
+
+    <div class="boader2" size="15"></div>
+
+    <img class="history"src="./newphoto/history.jpg" alt="history">
+    <img class="vision"src="./newphoto/vision.png" alt="vision">
+    <img class="mission"src="./newphoto/mission.jpg" alt="mission">
+
+    <div class="Habc">ABCD</div>
+    <div class="Vabc">ABCD</div>
+    <div class="Mabc">ABCD</div>
+</body>
+=======
+<!DOCTYPE html>
+<html>
+  <head>
+    
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./new.css" />
+<link rel="stylesheet" href="./global.css" />
+<script src="Jswebse.js"></script>
+
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gidugu:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gwendolyn:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Halant:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=IM FELL English:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;600;700&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Charm:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Fjalla One:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant Infant:wght@700&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cookie:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Josefin Slab:wght@700&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Habibi:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gupter:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant:wght@400&display=swap"/>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant Garamond:wght@400&display=swap"/>
+
+</head>
+
+<body>
+    <div class="boader" size="5"></div>
+    <a href="/Main.html">
+    <div class="shop">Shop</div>
+    </a>
+
+    <img class="search"src="./newphoto/search.png" alt="Search" sizes="-10">
+    <img class="marble"src="./newphoto/marble.png" alt="marbs">
+
+    <div class="trans" size="15"></div>
+    <a href="/Categories-Folder/Categories.html">
+    <div class="functionbut" size="15"></div></a>
+    
+    <div class="about">ABOUT TSCakes</div>
+    <div class="tagline">"Sweetness in Every Bite, Wonder in Every Slice"</div>
+
+    <div class="boader2" size="15"></div>
+
+    <img class="history"src="./newphoto/history.jpg" alt="history">
+    <img class="vision"src="./newphoto/vision.png" alt="vision">
+    <img class="mission"src="./newphoto/mission.jpg" alt="mission">
+
+    <div class="Habc">ABCD</div>
+    <div class="Vabc">ABCD</div>
+    <div class="Mabc">ABCD</div>
+</body>
+>>>>>>> 9a4a920da7720cb08a5ac661e49389e00be875dc
