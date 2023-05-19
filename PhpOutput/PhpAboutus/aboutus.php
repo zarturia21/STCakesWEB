6d61efa126fbdc44a8ee591b2f7f579eea362427
Zarturia <fallgame994@gmail.com>
2023-05-19 22:26:14 +0800

    sd

--- PhpOutput/PhpAboutus/aboutus.php
+++ PhpOutput/PhpAboutus/aboutus.php
@@ -4,10 +4,10 @@
     
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="./aboutus.css" />
+<link rel="stylesheet" href="./abouts.css" />
 <link rel="stylesheet" href="./global.css" />
-<script src="./aboutus.js"></script>
-
+<script src="aboutus.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gidugu:wght@400&display=swap"/>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gwendolyn:wght@400&display=swap"/>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Halant:wght@400&display=swap"/>
@@ -22,35 +22,34 @@
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gupter:wght@400&display=swap"/>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant:wght@400&display=swap"/>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Cormorant Garamond:wght@400&display=swap"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
 </head>
 
-<body>
-<div id="loader" class="center"></div>
 
     <div class="boader" size="5"></div>
-
     <div class="shop">Shop</div>
 
     <img class="search"src="./newphoto/search.png" alt="Search" sizes="-10">
     <img class="marble"src="./newphoto/marble.png" alt="marbs">
 
     <div class="trans" size="15"></div>
-    
-    <a href="http://localhost/STCakesWEB/PhpOutput/Categories-Folder/categ.php">
-  <div class="functionbut" size="15"></div>
-  </a>
-
     <div class="about">ABOUT TSCakes</div>
     <div class="tagline">"Sweetness in Every Bite, Wonder in Every Slice"</div>
 
     <div class="boader2" size="15"></div>
 
-    <img class="history"src="./newphoto/history.jpg" alt="history">
-    <img class="vision"src="./newphoto/vision.png" alt="vision">
+
+    <img class="history img-fluid" src="./newphoto/history.jpg" alt="history">
+    <div class="image-container">
+      <img class="vision" src="./newphoto/Vision.jpg" alt="vision">
+    </div>
+    
     <img class="mission"src="./newphoto/mission.jpg" alt="mission">
 
-    <div class="Habc">ABCD</div>
-    <div class="Vabc">ABCD</div>
-    <div class="Mabc">ABCD</div>
+    <div class="Habc">HISTORY</div>
+    <div class="Vabc">VISION</div>
+    <div class="Mabc">MISSION</div>
+
+    <img class="home"src="./newphoto/home.png" alt="home" sizes="-10">
 </body>
