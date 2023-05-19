52d29ee0a42cd1f9b09a6d407dac2c6e8d734604
Zarturia <fallgame994@gmail.com>
2023-05-19 23:50:58 +0800

    s

--- PhpOutput/PhpAboutus/aboutus.php
+++ PhpOutput/PhpAboutus/aboutus.php
@@ -5,8 +5,8 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="./abouts.css" />
-<link rel="stylesheet" href="./global.css" />
-<script src="aboutus.js"></script>
+<link rel="stylesheet" href="./globals.css" />
+<script src="abtus.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gidugu:wght@400&display=swap"/>
 <link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Gwendolyn:wght@400&display=swap"/>
@@ -28,7 +28,8 @@
 
 
     <div class="boader" size="5"></div>
-    <div class="shop">Shop</div>
+    <a href="http://localhost/STCakesWEB/PhpOutput/Categories-Folder/categ.php">
+    <div class="shop">Shop</div></a>
 
     <img class="search"src="./newphoto/search.png" alt="Search" sizes="-10">
     <img class="marble"src="./newphoto/marble.png" alt="marbs">
@@ -40,16 +41,25 @@
     <div class="boader2" size="15"></div>
 
 
-    <img class="history img-fluid" src="./newphoto/history.jpg" alt="history">
-    <div class="image-container">
-      <img class="vision" src="./newphoto/Vision.jpg" alt="vision">
-    </div>
-    
+    <img class="history" src="./newphoto/history.jpg" alt="history">
+    <img class="vision" src="./newphoto/Vision.jpg" alt="vision">
     <img class="mission"src="./newphoto/mission.jpg" alt="mission">
+    <a href="http://localhost/STCakesWEB/PhpOutput/AfterLogin/afterlog.php">
+    <img class="home"src="./newphoto/home.png" alt="home" sizes="-10"></a>
 
     <div class="Habc">HISTORY</div>
     <div class="Vabc">VISION</div>
     <div class="Mabc">MISSION</div>
 
-    <img class="home"src="./newphoto/home.png" alt="home" sizes="-10">
+   
+
+    <hr class="line1">
+
+    <img class="fb"src="./newphoto/fb.png" alt="fb" sizes="-10">
+    <img class="IG"src="./newphoto/IG.png" alt="IG" sizes="-10">
+    <img class="tik"src="./newphoto/tik.png" alt="tik" sizes="-10">
+    <img class="twit"src="./newphoto/twit.png" alt="twit" sizes="-10">
+    <img class="yt"src="./newphoto/yt.png" alt="yt" sizes="-10">
+
+    
 </body>
